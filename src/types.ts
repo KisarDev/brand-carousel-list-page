1e4cea59ebd3a40b79a469200b90c2e36d468892
unity-team-developers <cesarmartins.pro@gmail.com>
2024-11-08 12:16:40 -0300

    feat(types): typing datas like: mshops object and jsonMap

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Shop = {
+    id: string;
+    name: string;
+    accessibility_text: string;
+    url: string;
+    results: string;
+    img: string;
+  };
+
+export type Map = {
+    [key: string]: string;
+}
