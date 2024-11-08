726ac766d07b5bc9c7f305362c10a8b8c1564729
unity-team-developers <cesarmartins.pro@gmail.com>
2024-11-08 14:05:21 -0300

    feat(app): add rooks to control state of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"; // Importando os ganchos
 import { useZyph } from "usezyph";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
@@ -12,48 +13,57 @@ export default function App({ appId }: { appId: string }) {
   const [imgByBrand] = sdk.prop<Map>("imgByBrand");
   const [title] = sdk.prop<string>("title");
   const [slideConfig] = sdk.prop<any>("slideConfig");
-  const arrayShops: Shop[] = getData({ object: window });
-  // const objetoInterno: Record<string, string> = {
-  //   "Lavor": "url_da_img0",
-  //   "Util": "url_da_img1",
-  //   "Flash": "url_da_img2",
-  //   "Limp": "url_da_img3"
-  // }
-  arrayShops && imgByBrand && arrayShops.map(objetoShops => objetoShops.img = imgByBrand[`${objetoShops.name}`])
-  console.log(arrayShops)
+  const initialArrayShops: Shop[] = getData({ object: window });
+  const [arrayShops, setArrayShops] = useState<Shop[]>(initialArrayShops);
+  console.log("#Debug, Array inicial do shops sem as imagens", arrayShops)
+  console.log("#Debug, array e imgs", arrayShops, imgByBrand)
+  useEffect(() => {
+    if (imgByBrand && arrayShops) {
+      console.log("#Debug, entrei no if")
+      const updatedShops: Shop[] = arrayShops.map((objetoShops) => {
+        objetoShops.img = imgByBrand[`${objetoShops.name}`];
+        return objetoShops
+      });
+      setArrayShops(updatedShops);
+    }
+  }, [imgByBrand, arrayShops]);
+  console.log("#Debug, Array final com as imagens do json", arrayShops)
+
   return (
-    <div className='swiper mySwiper-destaques swiper-destaques ' id="carrosselDestaques">
+    <div className='swiper mySwiper-destaques swiper-destaques' id="carrosselDestaques">
       {title && (
         <h2 className="destaques-title">
-          <span>
-            {title}
-          </span>
+          <span>{title}</span>
         </h2>
       )}
       {slideConfig && (
-
-        <Swiper 
-        slidesPerView={3}
-        spaceBetween={10} 
-        navigation={true}
-        pagination={{ clickable: true }}  
-        loop={true} 
-        breakpoints={{ 
-          768: {
-            slidesPerView: slideConfig.itemsPerSlide || 5,
-            spaceBetween: slideConfig.spaceBetween || 10
-          },
-        }}
-        modules={[ Navigation, Pagination ]}
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={10}
+          navigation={true}
+          pagination={{ clickable: true }}
+          loop={true}
+          breakpoints={{
+            768: {
+              slidesPerView: slideConfig.itemsPerSlide || 5,
+              spaceBetween: slideConfig.spaceBetween || 10
+            },
+          }}
+          modules={[Navigation, Pagination]}
         >
-        {arrayShops && arrayShops.map((item: Shop, index: number) => (
-          <SwiperSlide key={index} className="swiper-slide">
-                <a href={item.url}>
-                  <img id={`image-destaques-${index}`} className="image-destaques" src={item.img} alt={item.name}/>
-                  <p className="swiper-item-title">{item.name}</p>
-                </a>
-          </SwiperSlide>
-            ))}
+          {arrayShops.map((item: Shop, index: number) => (
+            <SwiperSlide key={index} className="swiper-slide">
+              <a href={item.url}>
+                <img
+                  id={`image-destaques-${index}`}
+                  className="image-destaques"
+                  src={item.img}
+                  alt={item.name}
+                />
+                <p className="swiper-item-title">{item.name}</p>
+              </a>
+            </SwiperSlide>
+          ))}
         </Swiper>
       )}
     </div>
