c846851038abaa337ad047ae068fb6a371da0a14
unity-team-developers <cesarmartins.pro@gmail.com>
2024-11-08 12:15:39 -0300

    feat(data): adjust style of code

--- src/data.ts
+++ src/data.ts
@@ -4,15 +4,15 @@ function getData({ object }: { object: Window & typeof globalThis }) {
         if (object && "__PRELOADED_STATE__" in object) {
             const data = JSON.parse((object as any).__PRELOADED_STATE__.innerHTML);
             const components = data.pageState.initialState.sidebar.components
-            components.map((component: any)=> {
-                if(component.type == "AVAILABLE_FILTERS") {
+            components.map((component: any) => {
+                if (component.type == "AVAILABLE_FILTERS") {
                     const arrayBrands = component.filters
-                    arrayBrands.map((item:any) => {
-                        if(item.id == "BRAND") {
+                    arrayBrands.map((item: any) => {
+                        if (item.id == "BRAND") {
                             brands = item.values
                         }
                     }
-                )
+                    )
                     return brands
                 }
             }
